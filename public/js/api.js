2ed632289aff6c914912ca4bc5d8c7155ee19b63
ntijoh-jesper-tordebring-jansson <jesper.tordebringjansson@elev.ga.ntig.se>
2024-10-03 12:49:07 +0200

    api

--- public/js/api.js
+++ public/js/api.js
@@ -1,3 +1,19 @@
-async function index() {
-   console.log("implement me")
+class Api {
+
+   async loaddedd() {
+      const result = await index();
+      result.forEach(element => {
+         console.log(element.img)   
+      });
+   }
+
+   async index() {
+      const response = await fetch("http://localhost:9292/api/employees");
+      const result = await response.json();
+      return result;
+   }
+
+
 }
+
+export { Api }
