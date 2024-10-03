91b73de37f4b23ec03c4fe136eb73e01bb11c043
PhilipBranting05 <philip@agbilverkstad.se>
2024-10-03 15:38:28 +0200

    SPA done

--- public/js/card-board.js
+++ public/js/card-board.js
@@ -5,24 +5,56 @@ class CardBoard extends HTMLElement {
     super();
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(this.#template().content.cloneNode(true));
-    this.div = this.shadowRoot.querySelector("div");
-    console.log(new Api().index());
+    this.buttonDiv = this.shadowRoot.querySelector(".buttons");
+    this.contentDiv = this.shadowRoot.querySelector(".content");
+    this.showAllButton = this.shadowRoot.querySelector("#show-all");
+    this.tillbaksButton = this.shadowRoot.querySelector("#tillbaka");
   }
 
   async connectedCallback() {
-    const api = new Api();
-    const employees = await api.index();
+    this.showAllButton.addEventListener("click", () => {
+      this.#showAll();
+      this.buttonDiv.querySelectorAll(".startPage").forEach((element) => {
+        element.style.display = "none";
+      });
+      this.buttonDiv.querySelector("#tillbaka").style.display = "block";
+    });
+  }
+
+  async #showAll() {
+    this.tillbaksButton.addEventListener("click", () => {
+      this.contentDiv.innerHTML = "";
+      this.buttonDiv.querySelectorAll(".startPage").forEach((element) => {
+        element.style.display = "inline-block";
+      });
+      this.buttonDiv.querySelector("#tillbaka").style.display = "none";
+    });
+
+    const employees = await new Api().index();
     for (let i = 0; i < employees.length; i++) {
-      this.div.appendChild(new EmployeeCardCard(i));
+      this.contentDiv.appendChild(new EmployeeCard(i, employees[i]));
     }
   }
 
   #template() {
     const template = document.createElement("template");
     template.innerHTML = `
-      <div>
-      </div>
-            `;
+        <div class="buttons">
+          <button class="startPage" id="show-all">Visa alla</button>
+          <button class="startPage">Lägg till</button>
+          <button class="startPage">Ta bort</button>
+          <button class="startPage">Ändra</button>
+          <button id="tillbaka" style="display: none;">Tillbaka</button>
+        </div>
+
+        <div class="content">
+
+        </div>
+
+
+              `;
+
+    return template;
   }
 }
 
--- public/js/employee-card.js
+++ public/js/employee-card.js
@@ -1,22 +1,24 @@
 class EmployeeCard extends HTMLElement {
-  constructor(id) {
+  constructor(id, employee) {
     super();
     this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(this.#template().content.cloneNode(true));
     this.id = id;
+    this.employee = employee;
+    this.shadowRoot.appendChild(this.#template().content.cloneNode(true));
   }
 
   #template() {
     const template = document.createElement("template");
     template.innerHTML = `
-      <div class='employee card' data-id='${this.id}'">
-          <h1 class='name'></slot></h1>
-          <p class='email'></p>
-          <p class='phone'></p>
-          <p class='department_id'></p>
-          <img src=''>
+      <div class='employee card' data-id="${this.id}">
+          <h1 class='name'>${this.employee.name}</h1>
+          <p class='email'>${this.employee.email}</p>
+          <p class='phone'>${this.employee.phone}</p>
+          <p class='department_id'>${this.employee.department_id}</p>
+          <img src='img/${this.employee.img}.jpg'>
       </div>
             `;
+    return template;
   }
 }
 
