e1cac6186bc14c5b6657a85d53ef8ec442819d0d
ntijoh-jesper-tordebring-jansson <jesper.tordebringjansson@elev.ga.ntig.se>
2024-10-07 15:50:03 +0200

    fungerar

--- public/js/edit-employee.js
+++ public/js/edit-employee.js
@@ -13,27 +13,18 @@ export class EditEmployeeCard extends HTMLElement {
     this.empId;
   }
 
-
-
   connectedCallback() {
-    // Hantera formulärsubmitt
     this.form.addEventListener("submit", (e) => {
       e.preventDefault();
-
-      if (!this.empId) {
-        console.error("Ingen anställd vald, empId är undefined.");
-        return;
-      }
-
-      console.log('Ändrar anställd med id:', this.empId);
 
       const name = this.shadowRoot.querySelector("#name").value;
       const email = this.shadowRoot.querySelector("#email").value;
       const phone = this.shadowRoot.querySelector("#phone").value;
       const departmentId = this.shadowRoot.querySelector("#department_id").value;
 
-      console.log(this.empId);
-      this.#upload(name, email, phone, departmentId, this.empId);
+      const id = this.form.querySelector("img").getAttribute("class");
+
+      this.#upload(name, email, phone, departmentId, id);
 
       this.shadowRoot.querySelector("#name").value = "";
       this.shadowRoot.querySelector("#email").value = "";
@@ -41,136 +32,61 @@ export class EditEmployeeCard extends HTMLElement {
       this.shadowRoot.querySelector("#department_id").value = "";
     });
 
-    // Hantera klick på employee-card för att sätta empId och uppdatera formuläret
     this.parentNode.addEventListener("click", (e) => {
-      if (!e.target.shadowRoot) return; // Kontrollera om klicket var på ett element med shadow DOM
+      this.empId = e.target.shadowRoot
+        .querySelector("div")
+        .getAttribute("data-id");
+      console.log(this.empId);
 
-      const employeeDiv = e.target.shadowRoot.querySelector("div");
-      if (employeeDiv) {
-        this.empId = employeeDiv.getAttribute("data-id");
-        console.log("Vald empId:", this.empId);
-
-        this.infoList = []; // Rensa listan
-
-        e.target.shadowRoot
-          .querySelector(".employee-card")
-          .querySelector("div")
-          .childNodes.forEach((element) => {
-            if (element.nodeType !== Node.TEXT_NODE) {
-              this.infoList.push(element.innerHTML);
-            }
-          });
-
-        this.form
-          .querySelector("img")
-          .setAttribute(
-            "src",
-            e.target.shadowRoot
-              .querySelector(".employee-card")
-              .querySelector("img")
-              .getAttribute("src")
-          );
-
-        this.shadowRoot.querySelector("#name").value = this.infoList[0];
-        this.shadowRoot.querySelector("#email").value = this.infoList[1]
-          .split("Email: ")[1]
-          ?.trim();
-        this.shadowRoot.querySelector("#phone").value = this.infoList[2]
-          .split("Number: ")[1]
-          ?.trim();
-        this.shadowRoot.querySelector("#department_id").value = this.infoList[3]
-          .split("Department ID: ")[1]
-          ?.trim();
-
-        // Dölj andra employee-cards
-        this.parentNode.childNodes.forEach((element) => {
-          if (element.shadowRoot) {
-            element.style.display = "none";
+      e.target.shadowRoot
+        .querySelector(".employee-card")
+        .querySelector("div")
+        .childNodes.forEach((element) => {
+          if (element.nodeType == Node.TEXT_NODE) {
+          } else {
+            this.infoList.push(element.innerHTML);
           }
         });
 
-        // Visa redigeringsformuläret
-        this.style.display = "block";
-      }
-    });
+      this.form
+        .querySelector("img")
+        .setAttribute(
+          "src",
+          e.target.shadowRoot
+            .querySelector(".employee-card")
+            .querySelector("img")
+            .getAttribute("src"),
+        );
+
+      this.form
+      .querySelector("img")
+      .setAttribute(
+        "class",
+        e.target.shadowRoot
+          .querySelector(".employee-card")
+          .getAttribute("data-id"),
+      );
+
+      this.shadowRoot.querySelector("#name").value = this.infoList[0];
+      this.shadowRoot.querySelector("#email").value = this.infoList[1]
+        .split("Email: ")[1]
+        ?.trim();
+      this.shadowRoot.querySelector("#phone").value = this.infoList[2]
+        .split("Number: ")[1]
+        ?.trim();
+      this.shadowRoot.querySelector("#department_id").value = this.infoList[3]
+        .split("Department ID: ")[1]
+        ?.trim();
+
+      this.parentNode.childNodes.forEach((element) => {
+        if (element.shadowRoot) {
+          element.style.display = "none";
+        }
+      });
+
+      this.style.display = "block";
+    });  
   }
-
-
-
-  // connectedCallback() {
-  //   this.form.addEventListener("submit", (e) => {
-  //     e.preventDefault();
-
-  //     console.log('ändrarar');
-  //     console.log(e);
-
-  //     const name = this.shadowRoot.querySelector("#name").value;
-  //     const email = this.shadowRoot.querySelector("#email").value;
-  //     const phone = this.shadowRoot.querySelector("#phone").value;
-  //     const departmentId = this.shadowRoot.querySelector("#department_id").value;
-
-
-  //     console.log(this.empId);
-
-  //     // const id = e.getAttribute("data-id");
-
-  //     const id = this.empId;
-
-  //     console.log(id);
-  //     this.#upload(name, email, phone, departmentId, id);
-
-  //     this.shadowRoot.querySelector("#name").value = "";
-  //     this.shadowRoot.querySelector("#email").value = "";
-  //     this.shadowRoot.querySelector("#phone").value = "";
-  //     this.shadowRoot.querySelector("#department_id").value = "";
-  //   });
-
-  //   this.parentNode.addEventListener("click", (e) => {
-  //     this.empId = e.target.shadowRoot
-  //       .querySelector("div")
-  //       .getAttribute("data-id");
-  //     console.log(this.empId);
-
-  //     e.target.shadowRoot
-  //       .querySelector(".employee-card")
-  //       .querySelector("div")
-  //       .childNodes.forEach((element) => {
-  //         if (element.nodeType == Node.TEXT_NODE) {
-  //         } else {
-  //           this.infoList.push(element.innerHTML);
-  //         }
-  //       });
-
-  //     this.form
-  //       .querySelector("img")
-  //       .setAttribute(
-  //         "src",
-  //         e.target.shadowRoot
-  //           .querySelector(".employee-card")
-  //           .querySelector("img")
-  //           .getAttribute("src"),
-  //       );
-
-  //     this.shadowRoot.querySelector("#name").value = this.infoList[0];
-  //     this.shadowRoot.querySelector("#email").value = this.infoList[1]
-  //       .split("Email: ")[1]
-  //       ?.trim();
-  //     this.shadowRoot.querySelector("#phone").value = this.infoList[2]
-  //       .split("Number: ")[1]
-  //       ?.trim();
-  //     this.shadowRoot.querySelector("#department_id").value = this.infoList[3]
-  //       .split("Department ID: ")[1]
-  //       ?.trim();
-
-  //     this.parentNode.childNodes.forEach((element) => {
-  //       if (element.shadowRoot) {
-  //         element.style.display = "none";
-  //       }
-  //     });
-
-  //     this.style.display = "block";
-  //   });  
-  // }
 
 
 
